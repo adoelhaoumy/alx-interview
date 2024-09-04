1b1322365564dde6d5131a4d1226ea66325a2bd2
adoelhaoumy <alhoumi20@gmail.com>
2024-09-04 02:52:56 -0700

    island

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,31 +1,36 @@
 #!/usr/bin/python3
+"""
+This file defines the function calculate_island_perimeter.
+"""
 
-'''
-Island parameter
-'''
+
+def adjacent_land_cells(grid, x, y):
+    """calculates the number of adjacent
+    land cells."""
+    adjacent_count = 0
+    num_rows = len(grid)
+    num_columns = len(grid[0])
+    
+    if (x > 0 and grid[x-1][y] == 1):
+        adjacent_count += 1
+    if (y > 0 and grid[x][y-1] == 1):
+        adjacent_count += 1
+    if (x < num_rows-1 and grid[x+1][y] == 1):
+        adjacent_count += 1
+    if (y < num_columns-1 and grid[x][y+1] == 1):
+        adjacent_count += 1
+    
+    return adjacent_count
 
 
 def island_perimeter(grid):
-    '''
-    eturns the perimeter of the island described in grid
-    '''
-    rows = len(grid)
-    cols = len(grid[0])
-    perimeter = 0
-
-    for row in range(rows):
-        for col in range(cols):
-            if grid[row][col] == 1:
-                # count top edge
-                if row == 0 or grid[row-1][col] == 0:
-                    perimeter += 1
-                # count bottom edge
-                if row == rows - 1 or grid[row+1][col] == 0:
-                    perimeter += 1
-                # count left edge
-                if col == 0 or grid[row][col-1] == 0:
-                    perimeter += 1
-                # count right edge
-                if col == cols - 1 or grid[row][col+1] == 0:
-                    perimeter += 1
-    return perimeter
+    """returns the perimeter of an island
+    follwing the grid."""
+    perimeter_total = 0
+    
+    for x in range(len(grid)):
+        for y in range(len(grid[0])):
+            if grid[x][y] == 1:
+                perimeter_total += 4 - adjacent_land_cells(grid, x, y)
+    
+    return perimeter_total
